9d552eef51a5fff7350b6c37f79be378a81e816f
ss-zun <zxq0158@gmail.com>
2024-03-27 16:25:55 +0900

    Time 설정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject square;
+
+    public Text timeTxt;
+
+    float time = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +21,8 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2"); // 소수점 두 번째자리까지
     }
 
     void MakeSquare()

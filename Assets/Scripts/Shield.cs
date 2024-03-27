e5125e1e17ab769f13791cd0c441936c933682a9
ss-zun <zxq0158@gmail.com>
2024-03-27 16:07:16 +0900

    풍선, 쉴드, 스퀘어 충돌 및 스퀘어 랜덤생성

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -13,6 +13,10 @@ public class Shield : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 스크린상의 마우스 좌표를 카메라가 찍고 있는 월드 좌표로 바꾸기
+        // ScreenToWorldPoint() : 스크린의 위치를 게임월드상의 위치로 바꿔주는 함수
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // 마우스 위치를 쉴드 위치에 넣기
+        transform.position = mousePos;
     }
 }
---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = Random.Range(-3.0f, 3.0f);
+        float y = Random.Range(3.0f, 5.0f);
+
+        transform.position = new Vector2(x, y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

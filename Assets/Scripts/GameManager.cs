a03e756a38eb5f94bad988530c3a28bd63f6854e
ss-zun <zxq0158@gmail.com>
2024-04-01 14:26:08 +0900

    싱글톤 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,20 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public static GameManager Instance;
 
     public GameObject square;
     public Text timeTxt;
 
     float time = 0.0f;
+
+    private void Awake()
+    {
+        if(Instance == null) // Instance가 비어있을 때만
+        {
+            Instance = this;
+        }     
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -30,5 +38,10 @@ public class GameManager : MonoBehaviour
     {
         // 프리팹 생성
         Instantiate(square);
+    }
+
+    public void GameOver()
+    {
+
     }
 }

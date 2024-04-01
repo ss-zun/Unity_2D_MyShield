9dd5582b69a21cad858b528b95e74ac36e8e02f0
ss-zun <zxq0158@gmail.com>
2024-04-01 14:33:43 +0900

    GameOver 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,9 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     public GameObject square;
+    public GameObject endPanel;
     public Text timeTxt;
+    public Text nowScore;
 
     float time = 0.0f;
 
@@ -42,6 +44,8 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
-
+        Time.timeScale = 0.0f; // 타임의 크기 = 0 -> 멈춘것과 같음
+        nowScore.text = time.ToString("N2");
+        endPanel.SetActive(true);
     }
 }
--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -23,4 +23,12 @@ public class Square : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance.GameOver();
+        }
+    }
 }
